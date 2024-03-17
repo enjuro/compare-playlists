adfb7ab1f095901c64481db40af6949995bca540
enjuro <enjuro0926@gmail.com>
2024-03-17 13:53:06 +0900

    first commit

---
+++ main.py
@@ -0,0 +1,28 @@
+import streamlit as st
+# import numpy as np
+# import pandas as pd
+# from PIL import Image
+
+st.title("streamlitでSpotify")
+
+# st.write("dataframe")
+
+# df = pd.DataFrame({
+#     "1": [1, 2, 3, 4],
+#     "2": [12, 22, 32, 42]
+# })
+
+# st.dataframe(df)
+
+"""
+# タイトル
+#
+
+
+```
+import streamlit as st
+```
+"""
+
+# img = Image.open("./climb.JPG")
+# st.image(img, caption="登っている俺", use_column_width=True)
