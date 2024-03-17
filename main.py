5c8f4ed1a70b097f7e77a18a336ce75404d1f044
enjuro <enjuro0926@gmail.com>
2024-03-17 15:40:05 +0900

    search window

--- main.py
+++ main.py
@@ -1,18 +1,32 @@
 import streamlit as st
+
 # import numpy as np
 # import pandas as pd
 # from PIL import Image
+import module
 
-st.title("streamlitでSpotify")
 
-# st.write("dataframe")
 
-# df = pd.DataFrame({
-#     "1": [1, 2, 3, 4],
-#     "2": [12, 22, 32, 42]
-# })
+st.title("streamlitでSpotify APIを使う")
 
-# st.dataframe(df)
+
+# 入力フィールドを追加
+search_query = st.text_input("検索クエリ:")
+
+# 検索ボタンを追加
+search_button = st.button("検索")
+
+
+
+
+if search_button:
+    result = module.get_artist_image(search_query)
+    st.write(search_query)
+    st.image(result)
+
+
+
+
 
 """
 # タイトル
@@ -23,6 +37,17 @@ st.title("streamlitでSpotify")
 import streamlit as st
 ```
 """
+
+# st.write("dataframe")
+
+# df = pd.DataFrame({
+#     "1": [1, 2, 3, 4],
+#     "2": [12, 22, 32, 42]
+# })
+
+# st.dataframe(df)
+
+
 
 # img = Image.open("./climb.JPG")
 # st.image(img, caption="登っている俺", use_column_width=True)
---
+++ module.py
@@ -0,0 +1,32 @@
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
+
+sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials(client_id="f901ff2443e14b088a4ee57c94a35c72",
+                                                           client_secret="95ef6f1949d9478e8e48e2b397cb114a"))
+
+
+def get_songs_from_playlist(playlist_url):
+    # Extract playlist ID from the URL
+    playlist_id = playlist_url.split('/')[-1]
+
+    # Get playlist tracks
+    results = sp.playlist_tracks(playlist_id)
+    return results['items']
+
+
+def get_artist_image(artist_name):
+    # アーティストを検索してIDを取得
+    results = sp.search(q=artist_name, type='artist')
+    items = results['artists']['items']
+    if len(items) > 0:
+        artist_id = items[0]['id']
+
+        # アーティストの情報を取得して画像URLを取得
+        artist_info = sp.artist(artist_id)
+        if len(artist_info['images']) > 0:
+            image_url = artist_info['images'][0]['url']
+            return image_url
+        else:
+            return "No image available for this artist"
+    else:
+        return "Artist not found"
