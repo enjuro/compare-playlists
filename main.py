646eb2814f9a692e93aba908370f2e5cfabc772e
enjuro <enjuro0926@gmail.com>
2024-03-24 17:48:58 +0900

    playlist comparison

--- main.py
+++ main.py
@@ -6,46 +6,33 @@ import module
 
 st.title("Spotifyプレイリストの分析")
 
+search_query_1 = st.text_input("検索ワード1", "")
+search_query_2 = st.text_input("検索ワード2", "")
 
-search_query2 = st.text_input("プレイリストのURLを入力してください")
-
-search_button2 = st.button("楽曲情報取得")
-if search_button2:
+search_button = st.button("プレイリスト情報取得")
+if search_button:
     songs_df = pd.DataFrame()
 
-    songs = module.get_songs_from_playlist(search_query2)
+    songs_1 = module.get_songs_by_playlist_keyword(search_query_1)
+    songs_2 = module.get_songs_by_playlist_keyword(search_query_2)
 
-    for song in songs:
-        # タイトルを取得
-        song_title = song["track"]["name"]
-        # 楽曲の特徴を辞書型で取得
-        song_feature_dict = module.get_audio_features(song["track"]["uri"])
-        # 楽曲の特徴の辞書型をデータフレーム型に変換
-        song_feature_df = pd.DataFrame.from_dict(song_feature_dict)
-        # 数値のデータのみに限定
-        song_feature_df = song_feature_df.select_dtypes(include='float64')
-        song_feature_df.index = [song_title]
-        songs_df = pd.concat([songs_df, song_feature_df])
-        
-    st.dataframe(songs_df)
+    songs_1_df = module.create_song_df(songs_1)
+    songs_2_df = module.create_song_df(songs_2)
 
+    st.write(f"検索ワード：{search_query_1}")
+    st.write(songs_1_df.describe())
+
+    st.write(f"検索ワード：{search_query_2}")
+    st.write(songs_2_df.describe())
+    
+    # st.dataframe(songs_1_df)
+    # st.dataframe(songs_2_df)
 
     # for column in songs_df.columns:
-    #     st.write(column)
-    #     # 選択した列の要素をプロット
+    #     st.subheader(f'Histogram for {column}')
     #     plt.figure(figsize=(8, 6))
-    #     plt.bar(songs_df.index, songs_df[column])
-    #     plt.title(f'Plot of {column}')
-    #     plt.xlabel('Index')
-    #     plt.ylabel('Value')
-    #     plt.grid(True)
+    #     plt.hist(songs_df[column], bins=10, edgecolor='black')
+    #     plt.title(column)
+    #     plt.xlabel(column)
+    #     plt.ylabel('Frequency')
     #     st.pyplot(plt)
-
-    for column in songs_df.columns:
-        st.subheader(f'Histogram for {column}')
-        plt.figure(figsize=(8, 6))
-        plt.hist(songs_df[column], bins=10, edgecolor='black')
-        plt.title(column)
-        plt.xlabel(column)
-        plt.ylabel('Frequency')
-        st.pyplot(plt)
--- module.py
+++ module.py
@@ -1,36 +1,51 @@
+import pandas as pd
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
 
 sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials(client_id="f901ff2443e14b088a4ee57c94a35c72",
                                                            client_secret="95ef6f1949d9478e8e48e2b397cb114a"))
 
-# https://open.spotify.com/playlist/
-def get_songs_from_playlist(playlist_url):
+def search_playlists_by_keyword(keyword):
+    return  sp.search(q=keyword, type='playlist', limit=10)["playlists"]["items"]
+    # ids = list(map(lambda item: item["id"], playlist_items))
+    # return ids
+
+def get_songs_from_playlist(playlist_id):
     # Extract playlist ID from the URL
-    playlist_id = playlist_url.split('/')[-1]
+    playlist_id = playlist_id.split('/')[-1]
 
     # Get playlist tracks
-    results = sp.playlist_tracks(playlist_id, limit=10)
+    results = sp.playlist_tracks(playlist_id, limit=100)
     return results['items']
 
 
 def get_audio_features(song_id):
     return sp.audio_features(song_id)
 
+def create_song_df(songs):
+    songs_df = pd.DataFrame()
 
-def get_artist_image(artist_name):
-    # アーティストを検索してIDを取得
-    results = sp.search(q=artist_name, type='artist')
-    items = results['artists']['items']
-    if len(items) > 0:
-        artist_id = items[0]['id']
+    for song in songs:
+        # タイトルを取得
+        song_title = song["track"]["name"]
+        # 楽曲の特徴を辞書型で取得
+        song_feature_dict = sp.audio_features(song["track"]["id"])
+        # 楽曲の特徴の辞書型をデータフレーム型に変換
+        song_feature_df = pd.DataFrame.from_dict(song_feature_dict)
+        # 数値のデータのみに限定
+        song_feature_df = song_feature_df.select_dtypes(include='float64')
+        song_feature_df.index = [song_title]
+        songs_df = pd.concat([songs_df, song_feature_df])
 
-        # アーティストの情報を取得して画像URLを取得
-        artist_info = sp.artist(artist_id)
-        if len(artist_info['images']) > 0:
-            image_url = artist_info['images'][0]['url']
-            return image_url
-        else:
-            return "No image available for this artist"
-    else:
-        return "Artist not found"
+    return songs_df
+
+
+def get_songs_by_playlist_keyword(keyword):
+    playlist_items = sp.search(q=keyword, type='playlist', limit=10)["playlists"]["items"]
+    playlist_ids = list(map(lambda item: item["id"], playlist_items))
+
+    songs = []
+    for playlist_id in playlist_ids:
+        song_items = sp.playlist_tracks(playlist_id, limit=10)["items"]
+        songs += song_items
+    return songs
