9d3c327c9ca690f02af82dddfad81d3f252694c9
enjuro <enjuro0926@gmail.com>
2024-03-31 17:25:35 +0900

    完成

--- main.py
+++ main.py
@@ -1,40 +1,49 @@
 import streamlit as st
-import pandas as pd
 import matplotlib.pyplot as plt
 import module
 
+st.title("Spotifyプレイリストの比較分析ツール")
 
-st.title("Spotifyプレイリストの分析")
+'''
+2つの検索ワードを入力すると、そのワードからSpotify上のプレイリストを複数取得し、それらに含まれる楽曲をまとめ、特徴をヒストグラムに表示します。
+それぞれのキーワードから得られるプレイリストに含まれる曲が、どのように異なるのかを比較することができます。
+'''
+# 検索窓
+search_query_1 = st.text_input("検索ワード1", placeholder="例：ドライブ")
+search_query_2 = st.text_input("検索ワード2",  placeholder="例：リラックス")
 
-search_query_1 = st.text_input("検索ワード1", "")
-search_query_2 = st.text_input("検索ワード2", "")
-
+# 検索実行ボタン
 search_button = st.button("プレイリスト情報取得")
-if search_button:
-    songs_df = pd.DataFrame()
 
-    songs_1 = module.get_songs_by_playlist_keyword(search_query_1)
-    songs_2 = module.get_songs_by_playlist_keyword(search_query_2)
+# ボタンが押されたとき
+if search_button: 
+    # 2つの検索窓に何か入力されている場合は実行
+    if search_query_1 and search_query_2:
+        songs_1_df, playlist_titles_1 = module.create_song_df_from_keyword(search_query_1)
+        songs_2_df, playlist_titles_2 = module.create_song_df_from_keyword(search_query_2)
+
+        st.header("取得したデータの概要")
+
+        st.subheader(f"「{search_query_1}」で検索したプレイリストに含まれる楽曲の概要")
+        st.write(songs_1_df.describe())
+        st.write("取得したプレイリスト名　※各10曲抽出")
+        st.write(", ".join(playlist_titles_1))
+
+        st.subheader(f"「{search_query_2}」で検索したプレイリストに含まれる楽曲の概要")
+        st.write(songs_2_df.describe())
+        st.write("取得したプレイリスト名　※各10曲抽出")
+        st.write(", ".join(playlist_titles_2))
 
 
+        st.header("ヒストグラムでの各特長の比較")
+        for column in songs_1_df.columns:
+            st.subheader(f"{column}の比較")
 
-    songs_1_df = module.create_song_df(songs_1)
-    songs_2_df = module.create_song_df(songs_2)
-
-    st.write(f"検索ワード：{search_query_1}")
-    st.write(songs_1_df.describe())
-
-    st.write(f"検索ワード：{search_query_2}")
-    st.write(songs_2_df.describe())
-    
-    # st.dataframe(songs_1_df)
-    # st.dataframe(songs_2_df)
-
-    # for column in songs_df.columns:
-    #     st.subheader(f'Histogram for {column}')
-    #     plt.figure(figsize=(8, 6))
-    #     plt.hist(songs_df[column], bins=10, edgecolor='black')
-    #     plt.title(column)
-    #     plt.xlabel(column)
-    #     plt.ylabel('Frequency')
-    #     st.pyplot(plt)
+            fig, ax = plt.subplots()
+            plt.hist(songs_1_df[column], color="red", alpha=0.5)
+            plt.hist(songs_2_df[column], color="blue", alpha=0.5)
+            plt.legend([search_query_1, search_query_2], prop={"family": "MS Gothic"})
+            st.pyplot(fig)
+    # 入力欄のどちらかが空だったらアラート
+    else:
+        st.warning("2つの検索キーを入力してください")
--- module.py
+++ module.py
@@ -4,26 +4,28 @@ from spotipy.oauth2 import SpotifyClientCredentials
 
 sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials(client_id="f901ff2443e14b088a4ee57c94a35c72",
                                                            client_secret="95ef6f1949d9478e8e48e2b397cb114a"))
+def create_song_df_from_keyword(keyword):
+    
+        # キーワードからプレイリストを取得しそれらのidを配列に代入
+    playlist_items = sp.search(q=keyword, type='playlist', limit=2)["playlists"]["items"]
+    playlist_ids = list(map(lambda item: item["id"], playlist_items))
+    playlist_titles = list(map(lambda item: item["name"], playlist_items))
 
-def create_song_df(songs):
+
+    total_song_items = []
+    # プレイリストのidで曲情報を取得
+    for playlist_id in playlist_ids:
+        song_items = sp.playlist_tracks(playlist_id, limit=10)["items"]
+        total_song_items += song_items
+    
     # songs_df = pd.DataFrame()
-    song_ids = list(map(lambda item: item["track"]["id"], songs))
+    song_ids = list(map(lambda item: item["track"]["id"], total_song_items))
     # idのリストでfeatureを取得
     song_feature_dict = sp.audio_features(song_ids)
-
     song_feature_df = pd.DataFrame.from_dict(song_feature_dict)
     # 数値のデータのみに限定
     song_feature_df = song_feature_df.select_dtypes(include='float64')
 
-    return song_feature_df
+    return song_feature_df, playlist_titles
 
 
-def get_songs_by_playlist_keyword(keyword):
-    playlist_items = sp.search(q=keyword, type='playlist', limit=10)["playlists"]["items"]
-    playlist_ids = list(map(lambda item: item["id"], playlist_items))
-
-    songs = []
-    for playlist_id in playlist_ids:
-        song_items = sp.playlist_tracks(playlist_id, limit=10)["items"]
-        songs += song_items
-    return songs
